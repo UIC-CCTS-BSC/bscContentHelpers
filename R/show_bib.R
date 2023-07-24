6ac4e15f8a734d887c9e0d34e7c0b1a61fa7f9fd
Rachel Lane <rlane7@uic.edu>
2023-07-24 14:12:05 -0500

    adds fxn to strip latex symbols for html

--- R/show_bib.R
+++ R/show_bib.R
@@ -6,11 +6,16 @@
 #' @param name The basename of the .bib file
 #' @param dir The location of the .bib file. Absolute or relative to
 #'   the working directory.
+#' @param html Should the output be made html-safe?
 #'
 #' @return A formatted list of references. To show in an
 #'   RMarkdown document, set the chunk option `results='asis'`.
 #' @export
-show_bib <- function(name, dir = "files") {
+show_bib <- function(
+		name,
+		dir  = "files",
+		html = knitr::is_html_output()
+		) {
 	# make the file path
 	x = file.path(
 		dir,
@@ -20,6 +25,17 @@ show_bib <- function(name, dir = "files") {
 	if(!file.exists(x)) {
 		return(glue::glue("File not found: {x}"))
 		}
+
+	# make math symbols html-safe
+	if(html) {
+		t = tempfile(fileext = ".bib")
+
+		readLines(x) |>
+			latex_to_katex() |>
+			writeLines(con = t)
+
+		x = t
+	}
 
 	# read the bib file
 	x = RefManageR::ReadBib(x)
@@ -33,5 +49,21 @@ show_bib <- function(name, dir = "files") {
 			first.inits = FALSE,
 			sorting     = "nyt"
 		)
+	)
+}
+
+
+#' LaTeX to KaTeX
+#'
+#' @param text A string or vector of strings
+#'
+#' @return A string or vector of strings with html-safe math symbols
+#' @export
+latex_to_katex <- function(text) {
+	gsub(
+		"\\{\\$([^\\$]*)\\$\\}",
+		# " `r blogdown::shortcode_html('katex', .content = '\\1')` ",
+		"\\1",
+		text
 	)
 }
